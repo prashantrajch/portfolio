157e5303f58b38b7f1a90c6f42af1b9d875dfd26
prashantrajch <prashantraj9525@gmail.com>
2024-02-21 13:45:55 +0530

    script file add

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ menu.addEventListener('click', function(){
     this.classList.toggle('active')
     nav.classList.toggle('active');
 })
+
+function sendMail(e) {
+    if (
+      e.form.name.value != "" &&
+      e.form.email.value != "" &&
+      e.form.subject.value != "" &&
+      e.form.message.value != ""
+    ) {
+      let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+      };
+      emailjs
+        .send("service_55hgsvw", "template_d1ogy8g", params)
+        .then(alert("Email Sent....!"))
+    }
+  }
